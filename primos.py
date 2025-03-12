2f0005d5a9225bfb218d8b07b181b1e1e3d738c6
caballerodeark <126196836+caballerodeark@users.noreply.github.com>
2025-03-12 18:53:51 +0100

    Add primos.py
    Añado mi archivo plantilla (realizada a parte) para trabajarlo.

---
+++ primos.py
@@ -0,0 +1,50 @@
+# primos.py
+# Guillem Perez Sanchez QP 2024-25
+
+# APA-T2: Manejo de numeros primos
+
+import numpy as ny;
+
+# Determinación de la primalidad y descomposición de un número en factores primos
+
+def esPrimo(numero):
+  # Devuelve True si su argumento es primo, y False si no lo es.
+  return resultado;
+
+def primos(numero): 
+  # Devuelve una tupla con todos los números primos menores que su argumento.
+  return resultado;
+
+def descompon(numero):
+  # Devuelve una tupla con la descomposición en factores primos de su argumento.
+  return ressultado;
+
+# Obtención del mínimo común múltiplo y el máximo común divisor
+
+"""
+Estas dos funciones deben cumplir las condiciones siguientes:
+- Aunque se trate de una solución sub-óptima, en ambos casos 
+  deberá partirse de la descomposición en factores primos de 
+  los argumentos usando las funciones del apartado anterior.
+- Aunque también sea sub-óptimo desde el punto de vista de la 
+  programación, ninguna de las dos funciones puede depender de 
+  la otra; cada una debe programarse por separado.
+"""
+
+def mcm(numero1, numero2): 
+  # Devuelve el mínimo común múltiplo de sus argumentos.
+  return resultado;
+
+def mcd(numero1, numero2): 
+  # Devuelve el máximo común divisor de sus argumentos.
+  return resultado;
+
+# Obtención del mínimo común múltiplo y el máximo común divisor para un número arbitrario de argumentos
+
+def mcm(*numeros): 
+  # Devuelve el mínimo común múltiplo de sus argumentos.
+  return *resultados;
+
+def mcd(*numeros): 
+  # Devuelve el máximo común divisor de sus argumentos.
+  return *resultados;
